7f19fe8f4ef2f5d7ae0e2549a09735674abb0d50
Chris Lanoue <chris@nautilytics.com>
2020-03-27 13:44:18 -0400

    add play/pause buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,24 +2,32 @@ import React, { useEffect, useCallback } from 'react';
 import Spinner from './Spinner';
 import Tooltip from './Tooltip';
 import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import Maps from './Maps';
-import { retrieveData } from '../../redux/modules/global';
+import { retrieveData, TOGGLE_PLAY } from '../../redux/modules/global';
 
 const App = () => {
   const dispatch = useDispatch();
   const topology = useSelector(state => state.global.topology);
-  const roads = useSelector(state => state.global.roads);
   const data = useSelector(state => state.global.data);
+  const isPlaying = useSelector(state => state.global.isPlaying);
   const getData = useCallback(() => dispatch(retrieveData()), [dispatch]);
+  const updateIsPlaying = useCallback(() => dispatch({ type: TOGGLE_PLAY }), [dispatch]);
 
   useEffect(() => {
     getData();
   }, []);
 
+  const icon = isPlaying ? faPause : faPlay;
+  console.log(isPlaying, icon);
+  const onClick = () => updateIsPlaying();
+
   return (
     <div className="main">
       <Tooltip />
-      {topology && data.length && roads ? <Maps /> : <Spinner />}
+      <FontAwesomeIcon size="lg" className={`${isPlaying} ? 'pause' : 'play'} button`} icon={icon} onClick={onClick} />
+      {topology && data.length ? <Maps /> : <Spinner />}
     </div>
   );
 };
